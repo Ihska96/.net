eb9e013dd074b4e01f2ac34dc9e2dfa68f27947c
Ihska96 <112514403+Ihska96@users.noreply.github.com>
2023-10-08 04:04:44 +0530

    Add files via upload

---
+++ EnrollmentForm.aspx
@@ -0,0 +1,150 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EnrollmentForm.aspx.cs" Inherits="WebApp6Aug.EnrollmentForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body >
+    <form id="form1" runat="server">
+        <div>
+            <h1>Enrollment-Form</h1>
+            <table>
+                <tr>
+                    <td>
+                     NAME:
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                     FATHER_NAME:
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtfname" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                    MOTHER_NAME:
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtmname" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                     Gender:
+                    </td>
+                    <td>
+                        <asp:RadioButtonList ID="rblgender" runat="server" RepeatColumns="3">
+                        
+                        </asp:RadioButtonList>
+                    </td>
+                </tr>
+                     <tr>
+                    <td>
+                      Board:
+                    </td>
+                    <td>
+                        <asp:RadioButtonList ID="rblboard" runat="server" RepeatColumns="3">
+                          
+                        </asp:RadioButtonList>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                     Class:
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="ddclass" runat="server">
+                             
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                        Stream:
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="ddstream" runat="server" >
+                            
+                        </asp:DropDownList>               
+
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Button id="btnSave"  Text="Save" runat="server" OnClick="btnSave_Click"/>
+                    </td>
+                    <td>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                   <td>
+                       <asp:GridView ID="grd" runat="server" AutoGenerateColumns="false"  OnRowCommand="grd_RowCommand" >
+                          <Columns>
+                             <asp:TemplateField HeaderText="Student_Name">
+                             <ItemTemplate>
+                                 <%#Eval("NAME") %>
+                                 
+                             </ItemTemplate>
+                             </asp:TemplateField>
+
+                           <asp:TemplateField HeaderText="Fatrher_Name">
+                             <ItemTemplate>
+                                 <%#Eval("FATHER_NAME") %>
+                             </ItemTemplate>
+                         </asp:TemplateField>
+
+                         <asp:TemplateField HeaderText="Mother_Name">
+                             <ItemTemplate>
+                                 <%#Eval("MOTHER_NAME") %>
+                             </ItemTemplate>
+                         </asp:TemplateField>
+
+                         <asp:TemplateField HeaderText="Gender">
+                             <ItemTemplate>
+                                 <%#Eval("genderName") %>
+                             </ItemTemplate>
+                         </asp:TemplateField>
+                         <asp:TemplateField HeaderText="Board">
+                             <ItemTemplate>
+                                <%# Eval("bvalue") %>
+                             </ItemTemplate>
+                         </asp:TemplateField>
+                        
+                            <asp:TemplateField HeaderText="Class">
+                             <ItemTemplate>
+                                  <%# Eval("cname") %> 
+                             </ItemTemplate>
+                         </asp:TemplateField>
+
+                         <asp:TemplateField HeaderText="Stream">
+                             <ItemTemplate>
+                                 <%# Eval("sname")%>
+                             </ItemTemplate>
+                         </asp:TemplateField>
+                         <asp:TemplateField HeaderText="Actions">
+                             <ItemTemplate>
+                                <asp:LinkButton ID="btnDelete" runat="server" Text="Delete"  CommandName="del" CommandArgument='<%#Eval("EnrollNo") %>'/>
+                                <asp:LinkButton ID="Edi" runat="server" Text="Edit" CommandName="edi" CommandArgument='<%#Eval("EnrollNo")%>'/>
+                             </ItemTemplate>
+                         </asp:TemplateField>
+
+                      
+                     </Columns>
+                     </asp:GridView></td>
+                    
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EnrollmentForm.aspx.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.EnterpriseServices;
+using System.IO;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace WebApp6Aug
+{
+    public partial class EnrollmentForm : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("data source= DESKTOP-2OLVHPJ; initial catalog=Sep_23; integrated security=true");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+            if(!IsPostBack)
+            {
+                Display();
+                showGender();
+                showBoard();
+                SClass();
+                SStream();
+            }
+           
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+             
+        {
+            if (btnSave.Text == "Save")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into enrollments(NAME,FATHER_NAME,MOTHER_NAME,GENDER,BOARD,CLASS,STREAM) values('" + txtname.Text + "','" + txtfname.Text + "','" + txtmname.Text + "','" + rblgender.SelectedValue + "','" + rblboard.SelectedValue + "','" + ddclass.SelectedValue + "','" + ddstream.SelectedValue + "')", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Display();
+                txtname.Text = "";
+                txtfname.Text = "";
+                rblgender.ClearSelection();
+                rblboard.ClearSelection();
+                ddclass.ClearSelection();
+                ddstream.ClearSelection();
+
+            }
+            else if(btnSave.Text == "Update")
+            {
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand("update enrollments set NAME='" + txtname.Text + "' , FATHER_NAME='" + txtfname.Text + "' , MOTHER_NAME='" + txtmname.Text + "',GENDER='" + rblgender.SelectedValue + "', BOARD= '" + rblboard.SelectedValue + "', CLASS='" + ddclass.SelectedValue + "',STREAM='" + ddstream.SelectedValue + "'  where EnrollNo = '" + ViewState["abc"] + "' ", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Display();
+                txtname.Text = "";
+                txtfname.Text = "";
+                txtmname.Text = "";
+                rblgender.ClearSelection();
+                rblboard.ClearSelection();
+                ddclass.ClearSelection();
+                ddstream.ClearSelection();
+                btnSave.Text = "Save";
+
+
+            }
+
+
+        }
+
+        public void Display()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from enrollments join sgender on GENDER = genderid join board on BOARD = bid join class on CLASS= cid join stream on STREAM = sid", con);
+            
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            grd.DataSource = dt; 
+            grd.DataBind();
+        }
+
+        public void showGender()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select *from sgender", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            rblgender.DataValueField = "genderid";
+            rblgender.DataTextField = "gendername";
+            rblgender.DataSource = dt;
+            rblgender.DataBind();
+           
+
+        }
+
+
+        public void showBoard()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select *from board", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            rblboard.DataValueField = "bid";
+            rblboard.DataTextField = "bvalue";
+            rblboard.DataSource = dt;
+            rblboard.DataBind();
+        }
+
+        public void SClass()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select *from class", con);
+           SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            ddclass.DataValueField = "cid";
+            ddclass.DataTextField = "cname";
+            ddclass.DataSource = dt;
+            ddclass.DataBind();
+            ddclass.Items.Insert(0, new ListItem("--select Class---"));
+        }
+        public void SStream()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select *from stream", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            ddstream.DataValueField = "sid";
+            ddstream.DataTextField = "sname";
+            ddstream.DataSource = dt;
+            ddstream.DataBind();
+            ddstream.Items.Insert(0, new ListItem("--select stream---"));
+        }
+
+
+        protected void grd_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "del")
+            {
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand("delete from enrollments where EnrollNo='" + e.CommandArgument + "'", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Display();
+            }
+
+            else if (e.CommandName == "edi")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from enrollments where EnrollNo= '" + e.CommandArgument + "' ", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);            
+                con.Close(); 
+                txtname.Text = dt.Rows[0][1].ToString();
+                txtfname.Text = dt.Rows[0][2].ToString();
+                txtmname.Text = dt.Rows[0][3].ToString();
+                rblgender.SelectedValue = dt.Rows[0][4].ToString();
+                rblboard.SelectedValue = dt.Rows[0][5].ToString();
+                ddclass.SelectedValue = dt.Rows[0][6].ToString();
+                ddstream.SelectedValue = dt.Rows[0][7].ToString();
+
+                btnSave.Text = "Update";
+                ViewState["abc"] = e.CommandArgument;
+
+            }
+        }
+    }
+}
